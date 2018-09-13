5dc6a8049d7740b52421e759ffc126b70bf6f55e
shashank-singh2017 <shashank.singh@sjsu.edu>
2018-09-13 16:32:19 -0700

    new Ejs based structure

--- controllers/users.js
+++ controllers/users.js
@@ -5,26 +5,8 @@
  *
  */
 
+//var loginPage =require('../views/login.ejs');
+
 module.exports.login = function(req,res) {
-    var loginPage = '<DOCTYPE html>\n'
-                    + '<html lang="en-us">\n'
-                    + '<head>\n'
-                    + '   <meta charset="UTF-8">\n'
-                    + '   <title>Login to EvictionLand</title>\n'
-                    + '   <link rel="stylesheet" href="/stylesheets/login.css"\n'
-                    + '</head>\n'
-                    + '<body>\n'
-                    + '<div class="header"></div>\n'
-                    + '<div class="loginForm">\n'
-                    + '<input class="username" type="text" />\n'
-                    + '<br>\n'
-                    + '<input class="password" type="password" />\n'
-                    + '<br>\n'
-                    + '<button  class="okButton" name="submit">Login</button>\n'
-                    + '</div>\n'
-                    + '</body>\n'
-                    + '</html>\n';
-
-    res.send(loginPage);
-
+    res.render('../views/login');
 };
