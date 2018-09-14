4f59a764faa407d08f88f5c0d91f09e22d771c22
shashank-singh2017 <shashank.singh@sjsu.edu>
2018-09-14 14:52:57 -0700

    added home page and fixed conflicts

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var mainController = require('../controllers/home');
 
+/* GET home page. */
+router.get('/', mainController.home);
 
 module.exports = router;
